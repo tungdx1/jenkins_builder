a20161111b8e9574fc91b744961df7d4018af324
Đỗ Xuân Tùng <tungdx@trithucmoi.co>
2021-03-04 14:55:30 +0700

    init

---
+++ src/main/java/com/example/jenkins_builder/JenkinsBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins_builder/JenkinsBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
